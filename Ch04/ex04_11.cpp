197cadacc689d9a627b1dccae4af346ab614b6fc
wstfdxfh <91613525+wstfdxfh@users.noreply.github.com>
2025-01-03 18:41:06 +0800

    ex04_11

--- Ch04/ex04_11.cpp
+++ Ch04/ex04_11.cpp
@@ -36,9 +36,16 @@ int main(int argc, char const *argv[]) {
   usval + ival;  //  promotion depends on the size of unsigned short and int
   uival + lval;  //  conversion depends on the size of unsigned int and long
 
-  int i;
+  int i = 1, j = 2;
   int const &ri = i;  // const 指针或者引用可以指向或者引用非常量
+  void *p = &i;       // 任何非常量对象的地址都可以存放在void*指针中
+  // p = ri;             // 错误，不能将const int&转换为void*
   cout << ri << endl;
+  // 强制类型转换
+  double slope = static_cast<double>(i) / j;
+  cout << slope << endl;
+  double *dp = static_cast<double *>(p);
+  // const类型转换
 
   return 0;
 }
