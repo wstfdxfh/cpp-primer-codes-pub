995eb9ded9c7e39262e192d2e65453441db41813
wstfdxfh <91613525+wstfdxfh@users.noreply.github.com>
2025-02-09 15:16:46 +0800

    拷贝控制

--- Ch13/ch13_02.cpp
+++ Ch13/ch13_02.cpp
@@ -12,7 +12,31 @@ using namespace std;
 
 #include "ch13_02.h"
 
+HasPtr2::~HasPtr2() {
+  if (--*use == 0) {
+    delete ps;
+    delete use;
+  }
+}
+
+HasPtr2& HasPtr2::operator=(const HasPtr2& hasPtr) {
+  ++*hasPtr.use;
+  if (--*use == 0) {
+    delete ps;
+    delete use;
+  }
+  ps = hasPtr.ps;
+  i = hasPtr.i;
+  use = hasPtr.use;
+  return *this;
+}
+
 int main() {
   HasPtr p1("Hiya!");
+  HasPtr2 p2("Hello!");
+  HasPtr2 p22 = p2;
+  cout << *p22.ps << " " << *p22.use << endl;
+  (*p22.ps).append(", World!");
+  cout << *p2.ps << " " << *p2.use << endl;
   return 0;
 }
--- Ch13/ch13_02.h
+++ Ch13/ch13_02.h
@@ -10,7 +10,7 @@ class HasPtr {
   ~HasPtr() { delete ps; };
   HasPtr &operator=(const HasPtr &);
 
- private:
+  //  private:
   std::string *ps;
   int i;
 };
@@ -22,5 +22,22 @@ HasPtr &HasPtr::operator=(const HasPtr &hasPtr) {
   i = hasPtr.i;
   return *this;
 }
+// 行为像指针的类
+class HasPtr2 {
+ public:
+  HasPtr2(const std::string &s = std::string())
+      : ps(new std::string(s)), i(0), use(new std::size_t(1)) {}
+  HasPtr2(const HasPtr2 &hasPtr) : ps(hasPtr.ps), i(hasPtr.i), use(hasPtr.use) {
+    ++*use;
+  }
+  HasPtr2 &operator=(const HasPtr2 &);
+  ~HasPtr2();
+
+  //  private:
+  std::string *ps;
+  int i;
+  // 用于记录多少对象共享*ps的成员
+  std::size_t *use;
+};
 
 #endif
