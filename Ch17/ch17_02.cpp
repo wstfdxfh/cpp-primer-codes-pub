72710084d2dcb1300d350fe9bc108bd3dc8893ee
wstfdxfh <91613525+wstfdxfh@users.noreply.github.com>
2025-03-14 15:52:45 +0800

    17_02

--- Ch17/ch17_02.cpp
+++ Ch17/ch17_02.cpp
@@ -4,4 +4,16 @@
 #include <iostream>
 #include <string>
 
-int main() { return 0; }
+using namespace std;
+int main() {
+  bitset<64> bitvec(~0UL);
+  cout << "bitvec: " << bitvec << endl;
+
+  string str("00110011");
+  bitset<64> bitvec2(str);
+  bitset<64> bitvec3(str, 5, 2);
+  cout << "bitvec2: " << bitvec2 << endl;
+  cout << "bitvec3: " << bitvec3 << endl;
+
+  return 0;
+}
